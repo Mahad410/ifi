6dd246e4464fb67c5b7e28a142a394c0fb603ef1
Mahad410 <mahadqureshi410@gmail.com>
2021-12-17 23:05:47 -0600

    ifi

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var scroll_pos = 0;
     $(document).scroll(function() { 
         scroll_pos = $(this).scrollTop();
-        if(scroll_pos >600) {
+        if(scroll_pos >200) {
             $("nav").css('background-color', '#8f5575');
             $('nav a svg').css('color',"#fff");
         }
